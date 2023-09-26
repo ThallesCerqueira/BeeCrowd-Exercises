5a8c4235aa2d956b3a4318228ce7b9ca731620c8
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-09-26 14:47:17 -0300

    resolvendo BeeCrod 1014

---
+++ src/src/Bee1014.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int distancia;
+    double combustivel, consumo;
+
+    scanf("%d", &distancia);
+    scanf("%lf", &combustivel);
+
+    consumo = distancia / combustivel;
+
+    printf("%.3lf km/l\n", consumo);
+
+    return 0;
+}
