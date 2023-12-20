7663ef848a4d05e521f9ea16263b5be88166ee94
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-12-20 09:18:29 -0300

    Bee1099 finished

--- src/src/Bee1099.c
+++ src/src/Bee1099.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 
+int menor(int a, int b);
+
 int main() {
 
-    int n;
+    int n, x, y, qtd, menor, maior;
+    qtd = 0;
 
+    scanf("%d", &n);
 
+    for(int i = 0; i < n; i++) {
+
+        scanf("%d %d", &x, &y);
+
+        if(x == y) {
+            printf("0\n");
+        }else {
+
+            if(x < y) {
+                menor = x + 1;
+                maior = y - 1;
+            } else {
+                menor = y + 1;
+                maior = x - 1;
+            }
+
+            while(menor <= maior){
+
+                if(menor % 2 == 1) {
+                    qtd += menor;
+                }
+
+                menor++;
+
+            }
+
+            printf("%d\n", qtd);
+            qtd = 0;
+        }
+
+    }
 
     return 0;
+}
+
+int menor(int a, int b) {
+
+    if(a < b) return a;
+
+    return b;
+
 }
